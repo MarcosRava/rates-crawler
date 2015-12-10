ff4e85019613a7c3b879312d4dbc7c355353e219
Marcos Vieira da Silva <mvieira@rico.com.vc>
2015-12-10 17:55:11 -0200

    change bcb selic to parse file instead of html table

--- src/rates-sources/selic/bcb.js
+++ src/rates-sources/selic/bcb.js
@@ -1,30 +1,42 @@
 /*jslint node: true */
 "use strict";
 
-var url = 'http://www3.bcb.gov.br/selic/consulta/taxaSelic.do?method=listarTaxaDiaria';
+var url = 'http://www3.bcb.gov.br/selic/consulta/taxaSelic.do?method=listarTaxaDiaria&tipoApresentacao=arquivo';
 var request = require('request');
 var cheerio = require('cheerio');
 var Q = require('q');
 var extend = require('extend');
 var moment = require('moment');
 var result, dateFormat, rateType, source, options,
-  deferred, html, $, values, date,
-  rate, dailyFactor, calculationBasis, statistics;
+  deferred, html, $, date, split, content,
+  rate, dailyFactor, calculationBasis, statistics, indexes;
+
+indexes = {
+  DATE: 0,
+  RATE: 1,
+  DAILY_FACTOR: 2,
+  CALCULATION_BASIS: 3,
+  AVERAGE: 4,
+  MEDIAN: 5,
+  MODA: 6,
+  STANDART_DEVIATION: 7,
+  KURTOSI_INDEX: 8
+};
 
 function get(requestOptions) {
-  
+
   dateFormat = "DD/MM/YYYY";
   rateType = "selic";
   source = "Banco Central Do Brasil";
   //&dataInicial=27/08/2015&dataFinal=27/08/2015 //TODO
   options = {
-    date: moment().add(-1).toDate()
+    date: moment().add(1).toDate()
   };
-  
+
   options = extend(true, options, requestOptions);
-  
+
   deferred = Q.defer();
-  
+
   request(url, function (err, response, body) {
     if (err || (response && response.statusCode > 300)) {
       deferred.reject({error: true, errorMsg: response ? response.body : err});
@@ -32,37 +44,22 @@ function get(requestOptions) {
     }
     html = body
       .replace(/\r?\n/g, "");
-    
-    $ = cheerio.load(html, {
-      normalizeWhitespace: false,
-      xmlMode: false,
-      decodeEntities: true
-    });
-    
-    values = $('.tabelaTaxaSelic tr').eq(2).children('td');
-    date = values.eq(0).text();
-    
-    if (options.date && moment(options.date).format(dateFormat) !== date) {
-      return deferred.reject({
-        error: true,
-        rateType: rateType,
-        source: source,
-        msg: "date " + options.date + " dont match"
-      });
-    }
-    
+    split = html.split(';');
+    content = split.slice(split.length - 10, split.length - 1);
+
+    date = content[indexes.DATE];
     date = moment(date, dateFormat);
-    rate = parseFloat(values.eq(1).text().replace(/\./g, '').replace(/,/g, '.'));
-    dailyFactor = parseFloat(values.eq(2).text().replace(/\./g, '').replace(/,/g, '.'));
-    calculationBasis = parseFloat(values.eq(3).text().replace(/\./g, '').replace(/,/g, '.'));
+    rate = parseFloat(content[indexes.RATE].replace(/\./g, '').replace(/,/g, '.'));
+    dailyFactor = parseFloat(content[indexes.DAILY_FACTOR].replace(/\./g, '').replace(/,/g, '.'));
+    calculationBasis = parseFloat(content[indexes.CALCULATION_BASIS].replace(/\./g, '').replace(/,/g, '.'));
     statistics = {
-      average: parseFloat(values.eq(4).text().replace(/\./g, '').replace(/,/g, '.')),
-      median: parseFloat(values.eq(5).text().replace(/\./g, '').replace(/,/g, '.')),
-      moda: parseFloat(values.eq(6).text().replace(/\./g, '').replace(/,/g, '.')),
-      standardDeviation: parseFloat(values.eq(7).text().replace(/\./g, '').replace(/,/g, '.')),
-      kurtosiIndex: parseFloat(values.eq(8).text().replace(/\./g, '').replace(/,/g, '.'))
+      average: parseFloat(content[indexes.AVERAGE].replace(/\./g, '').replace(/,/g, '.')),
+      median: parseFloat(content[indexes.MEDIAN].replace(/\./g, '').replace(/,/g, '.')),
+      moda: parseFloat(content[indexes.MODA].replace(/\./g, '').replace(/,/g, '.')),
+      standardDeviation: parseFloat(content[indexes.STANDART_DEVIATION].replace(/\./g, '').replace(/,/g, '.')),
+      kurtosiIndex: parseFloat(content[indexes.KURTOSI_INDEX].replace(/\./g, '').replace(/,/g, '.'))
     };
-    
+
     result = {
       date: date.toDate(),
       rate: rate,
@@ -73,10 +70,10 @@ function get(requestOptions) {
       source: source,
       error: false
     };
-    
+
     deferred.resolve(result);
   });
-  
+
   return deferred.promise;
 }
 
